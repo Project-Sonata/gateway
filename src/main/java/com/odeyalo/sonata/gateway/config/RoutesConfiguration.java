1d2c17000b4135291635d71aac6dc2d40e116829
odeyalo <theprogrammerinfuture@gmail.com>
2025-10-14 23:12:12 +0300

    Add logging

--- src/main/java/com/odeyalo/sonata/gateway/config/RoutesConfiguration.java
+++ src/main/java/com/odeyalo/sonata/gateway/config/RoutesConfiguration.java
@@ -1,13 +1,13 @@
 package com.odeyalo.sonata.gateway.config;
 
-import com.odeyalo.sonata.gateway.filter.AuthenticationGatewayFilter;
 import com.odeyalo.sonata.gateway.route.RouteRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 
 import java.util.List;
 
@@ -16,18 +16,24 @@ public class RoutesConfiguration {
     @Autowired
     List<RouteRecord> routeRecords;
 
+    private final Logger logger = LoggerFactory.getLogger(RoutesConfiguration.class);
+
     @Bean
     public RouteLocator customRouteLocator(final RouteLocatorBuilder builder) {
 
         final RouteLocatorBuilder.Builder routeLocatorBuilder = builder.routes();
 
         routeRecords.forEach(record -> {
+
+            logger.info("Applying {} to {}", record.getRoute(), routeLocatorBuilder);
+
             routeLocatorBuilder.route(
                     record.getRoute().name(),
                     record.getRoute().fn()
             );
         });
 
+        logger.info("Successfully applied {} route records to {}", routeRecords.size(), routeLocatorBuilder);
 
         return routeLocatorBuilder.build();
     }
